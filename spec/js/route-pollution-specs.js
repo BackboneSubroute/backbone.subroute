8c97c2b6a58ccd812c1aecc135b1f9126efc18c5
Joshua Balloch <joshuaballoch@gmail.com>
2013-09-23 17:28:37 +0800

    add spec tests to test for polluted routes when initializing multiple routers from same class

---
+++ spec/js/route-pollution-specs.js
@@ -0,0 +1,21 @@
+describe("When initializing multiple routes from the same class", function() {
+
+    var subRouter = Backbone.SubRoute.extend( {
+        routes: {
+            "":"handDefaultRoute"
+        }
+    } );
+
+    it ( 'should have normal subroutes if just one subroute is initialized', function () {
+        SubRouterOne = new subRouter("subrouterone");
+        expect(SubRouterOne.routes).toEqual( { '' : 'handDefaultRoute', subrouterone : 'handDefaultRoute' } );
+    } );
+
+    it ( 'should not cross pollute routes', function () {
+        SubRouterOne = new subRouter("subrouterone");
+        SubRouterTwo = new subRouter("subroutertwo");
+        expect(SubRouterOne.routes).toEqual( { '' : 'handDefaultRoute', subrouterone : 'handDefaultRoute' } );
+        expect(SubRouterTwo.routes).toEqual( { '' : 'handDefaultRoute', subroutertwo : 'handDefaultRoute' } );
+    } );
+
+} );
